08122dddb5f5a0b5162dea5f284529d2fc805539
Kwippy <speedemon1132@yahoo.com>
2017-01-13 13:25:54 -0500

    1/13/2017
    Drive train subsystem

--- src/org/usfirst/frc/team4121/robot/subsystems/DriveTrainSubsystem.java
+++ src/org/usfirst/frc/team4121/robot/subsystems/DriveTrainSubsystem.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team4121.robot.subsystems;
 
+import com.ctre.CANTalon;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -10,9 +12,17 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class DriveTrainSubsystem extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
+	CANTalon leftMotor1 = new CANTalon(1);
+	CANTalon leftMotor2 = new CANTalon(1);
+	CANTalon rightMotor1 = new CANTalon(1);
+	CANTalon rightMotor2 = new CANTalon(1);
+	
+	RobotDrive = new RobotDrive(leftMotor1, leftMotor2, rightMotor1, rightMotor2);
 
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
+		
+		setDefaultCommand(new )
 	}
 }
