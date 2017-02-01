233bf687f2929914584495c0ec525c5346f8b753
Kwippy <speedemon1132@yahoo.com>
2017-01-31 20:19:33 -0500

    1/31/2017
    Added talons to RobotMap

--- src/org/usfirst/frc/team4121/robot/RobotMap.java
+++ src/org/usfirst/frc/team4121/robot/RobotMap.java
@@ -9,14 +9,18 @@ package org.usfirst.frc.team4121.robot;
  * @author Ben Hayden
  */
 public class RobotMap {
-	
-	//Mapping all the talons to the correct ports also use 172.22.11.2
 	public static final int LEFT_MOTOR_1 = 0;
 	public static final int LEFT_MOTOR_2 = 1;
 	public static final int LEFT_MOTOR_3 = 2;
 	public static final int RIGHT_MOTOR_1 = 3;
 	public static final int RIGHT_MOTOR_2 = 4;
 	public static final int RIGHT_MOTOR_3 = 5;
+	
+	public static final int CLIMBER = 100;
+	
+	public static final int FEEDER = 100;
+	
+	public static final int SHOOTER = 100;
 	
 	public static final double DRIVE_SPEED = 1.0;
 	
